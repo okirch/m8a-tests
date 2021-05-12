6a00e1024490b6b7ac5d623de9f2bc76f74145ba
Jean-Philippe Evrard <jean-philippe@evrard.me>
2021-05-12 14:17:06 +0200

    Add tests for new data
    Without this patch, we don't even test if the json is valid.
    
    This should fix it, and at the same time, cleans up remnants of
    old, unused, tests.

---
+++ test_unit.py
@@ -0,0 +1,20 @@
+import pytest
+import tempfile
+import os
+
+from matryoshka_tester.parse_data import build_containerlist
+
+
+def test_default_containerlist():
+    # assert len makes sure we are reading the default file
+    # to check for any eventual TypeErrors
+    assert len(build_containerlist()) != 0
+
+
+def test_buildcontainerlist():
+    with tempfile.TemporaryDirectory() as td:
+        tmpfile = os.path.join(td, "temp.json")
+        with open(tmpfile, "w") as fw:
+            fw.write("{}")
+        with pytest.raises(TypeError):
+            build_containerlist(tmpfile)
